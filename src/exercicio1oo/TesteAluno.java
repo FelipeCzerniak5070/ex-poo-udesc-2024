04d3ad750a6c708271518f86b01df24f16ee2fc6
FelipeCzerniak5070 <felipeczerniak@gmail.com>
2024-08-27 22:52:01 -0300

    Adicionada a classe Aluno

--- src/exercicio1oo/TesteAluno.java
+++ src/exercicio1oo/TesteAluno.java
@@ -1,2 +1,15 @@
-package exercicio1oo;public class TesteAluno {
+package exercicio1oo;
+
+public class TesteAluno {
+    public static void main(String[] args) {
+        Aluno a1 = new Aluno();
+        a1.nome="Gabriel Machado";
+        a1.idade=19;
+        a1.n1=9.0;
+        a1.n2=9.0;
+        a1.n3=9.0;
+        a1.n4=9.0;
+        a1.verificarSituacao();
+        System.out.println("Média: "+a1.calcularMedia());
+    }
 }
