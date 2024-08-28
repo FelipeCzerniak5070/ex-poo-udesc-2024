e849cb073f59627d01ed8bcea35273b50ed034e4
FelipeCzerniak5070 <felipeczerniak@gmail.com>
2024-08-27 23:42:30 -0300

    Commit final/Adicionada a classe Livro

---
+++ src/exercicio1oo/Livro.java
@@ -0,0 +1,22 @@
+package exercicio1oo;
+
+public class Livro {
+    public String nome, autor;
+    public boolean disponivel = true;
+
+    public boolean isDisponivel() {
+        return disponivel;
+    }
+
+    public void emprestar(){
+        disponivel=false;
+        System.out.println("Livro emprestado");
+    }
+
+    public void devolver(){
+        disponivel=true;
+        System.out.println("Livro Devolvido");
+    }
+
+
+}
