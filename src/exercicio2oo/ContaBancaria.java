441cb2a61021d9b284fee7c2b9b5fb908d1e8ef1
FelipeCzerniak5070 <felipeczerniak@gmail.com>
2024-09-15 17:10:58 -0300

    Atualização ContaBancaria, Getters e Setters

--- src/exercicio2oo/ContaBancaria.java
+++ src/exercicio2oo/ContaBancaria.java
@@ -1,8 +1,8 @@
 package exercicio2oo;
 
 public class ContaBancaria {
-    public String proprietario;
-    public double saldo;
+    private String proprietario;
+    private double saldo;
 
     public void sacar(double valor){
         saldo-=valor;
@@ -14,7 +14,21 @@ public class ContaBancaria {
         System.out.println("Você depositou "+valor);
     }
 
-    public double consultarSaldo(){
+    public String getProprietario() {
+        return proprietario;
+    }
+
+    public void setProprietario(String proprietario) {
+        this.proprietario = proprietario;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public double getSaldo(){
         return saldo;
     }
+
+
 }
--- src/exercicio2oo/TesteContaBancaria.java
+++ src/exercicio2oo/TesteContaBancaria.java
@@ -3,9 +3,9 @@ package exercicio2oo;
 public class TesteContaBancaria {
     public static void main(String[] args) {
         ContaBancaria c1 = new ContaBancaria();
-        c1.proprietario="Felipe";
-        c1.saldo=4000.0;
+        c1.setProprietario("Felipe");
+        c1.setSaldo(4000.0);
         c1.depositar(100);
-        System.out.println("Saldo: "+c1.consultarSaldo());
+        System.out.println("Saldo: "+c1.getSaldo());
     }
 }
