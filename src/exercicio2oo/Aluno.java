152a2c357cf476853a1a7e8837770d93c92c3661
FelipeCzerniak5070 <felipeczerniak@gmail.com>
2024-09-15 16:53:16 -0300

    Atualização Aluno, Getters e Setters

--- src/exercicio2oo/Aluno.java
+++ src/exercicio2oo/Aluno.java
@@ -1,8 +1,9 @@
 package exercicio2oo;
 public class Aluno {
-    public String nome;
-    public int idade;
-    public double n1, n2, n3, n4;
+    private String nome;
+    private int idade;
+    private double n1, n2, n3, n4;
+
     public double calcularMedia() {
         return (this.n1+this.n2+this.n3+this.n4)/4;
     }
@@ -15,4 +16,51 @@ public class Aluno {
         }
     }
 
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public double getN1() {
+        return n1;
+    }
+
+    public void setN1(double n1) {
+        this.n1 = n1;
+    }
+
+    public double getN2() {
+        return n2;
+    }
+
+    public void setN2(double n2) {
+        this.n2 = n2;
+    }
+
+    public double getN3() {
+        return n3;
+    }
+
+    public void setN3(double n3) {
+        this.n3 = n3;
+    }
+
+    public double getN4() {
+        return n4;
+    }
+
+    public void setN4(double n4) {
+        this.n4 = n4;
+    }
 }
--- src/exercicio2oo/TesteAluno.java
+++ src/exercicio2oo/TesteAluno.java
@@ -3,12 +3,12 @@ package exercicio2oo;
 public class TesteAluno {
     public static void main(String[] args) {
         Aluno a1 = new Aluno();
-        a1.nome="Gabriel Machado";
-        a1.idade=19;
-        a1.n1=9.0;
-        a1.n2=9.0;
-        a1.n3=9.0;
-        a1.n4=9.0;
+        a1.setNome("Gabriel Machado");
+        a1.setIdade(19);
+        a1.setN1(9.0);
+        a1.setN2(9.0);
+        a1.setN3(9.0);
+        a1.setN4(9.0);
         a1.verificarSituacao();
         System.out.println("Média: "+a1.calcularMedia());
     }
