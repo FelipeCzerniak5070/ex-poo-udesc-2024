f99575ee1e35f33d84b3a9b21da7c62bdc742bf3
FelipeCzerniak5070 <felipeczerniak@gmail.com>
2024-09-15 17:03:03 -0300

    Atualização Retangulo, Getters e Setters

--- src/exercicio2oo/Retangulo.java
+++ src/exercicio2oo/Retangulo.java
@@ -1,8 +1,24 @@
 package exercicio2oo;
 
 public class Retangulo {
-    public double width, height;
+    private double width, height;
     public double calcularArea(){
         return width*height;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
     }
 }
--- src/exercicio2oo/TesteRetangulo.java
+++ src/exercicio2oo/TesteRetangulo.java
@@ -3,8 +3,8 @@ package exercicio2oo;
 public class TesteRetangulo {
     public static void main(String[] args) {
         Retangulo r1 = new Retangulo();
-        r1.height=9.0;
-        r1.width=8.0;
+        r1.setHeight(9.);
+        r1.setWidth(10.);
         System.out.println("Área: "+r1.calcularArea());
 
     }
